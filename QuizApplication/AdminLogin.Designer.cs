20e6cad3fb1928151441459594a5974f2ed92aac
M.FAISAL AMIN <49854367+Faisal37150@users.noreply.github.com>
2021-01-16 21:45:17 +0500

    Update AdminLogin.Designer.cs
    Design Updated

--- QuizApplication/AdminLogin.Designer.cs
+++ QuizApplication/AdminLogin.Designer.cs
@@ -47,7 +47,7 @@
             this.groupBox1.Controls.Add(this.textBox1);
             this.groupBox1.Location = new System.Drawing.Point(12, 33);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(312, 307);
+            this.groupBox1.Size = new System.Drawing.Size(400, 307);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "groupBox1";
